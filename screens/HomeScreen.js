c29b69209bddc3c5418654a08091967d712a9961
Sadiq <seeker99157@gmail.com>
2022-11-14 19:07:56 +0100

    done with trek am build

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -21,94 +21,93 @@ const HomeScreen = () => {
 
 
     return (
-        <ScrollView >
-            <View style={tw`px-5 bg-white flex-1`}>
+
+        <View style={tw`px-5 bg-white flex-1`}>
+            <View>
+                {/* Logo */}
                 <View>
-                    {/* Logo */}
-                    <View>
-                        <Icon
-                            name="activity"
-                            color={"white"}
-                            type="feather"
-                            size={60}
-                            style={tw`p-2 bg-gray-700 rounded-full w-20 text-2xl mt-10 mb-2`}
-                        />
-                    </View>
-
-                    {/* Header Text */}
-                    <View>
-                        <Text style={tw`text-3xl font-bold pt-6 text-gray-700`}>Trek Am?</Text>
-                        <Text style={tw`text-xl pt-2 text-gray-700`}>Measure the distance</Text>
-                    </View>
+                    <Icon
+                        name="activity"
+                        color={"white"}
+                        type="feather"
+                        size={60}
+                        style={tw`p-2 bg-gray-700 rounded-full w-20 text-2xl mt-10 mb-2`}
+                    />
                 </View>
 
-
-                {/* Where from */}
-                <View style={tw`mt-4`}>
-                    <Text style={tw`text-xl text-gray-700 font-bold`}>Starting from?</Text>
-                    <View style={tw`mt-4 px-2`}>
-                        <GooglePlacesAutocomplete
-                            placeholder='Where From'
-                            nearbyPlacesAPI='GooglePlacesSearch'
-                            debounce={400}
-                            styles={inputBoxStyles}
-
-                            query={{
-                                key: 'AIzaSyBiH_uMgVwEkiVbs - kvSPEDd_NRzDzMdCE',
-                                language: 'en'
-                            }}
-                            minLength={2}
-                            enablePoweredByContainer={false}
-                            onPress={(data, details = null) => {
-                                dispatch(setOrigin({
-                                    location: details.geometry.location,
-                                    description: data.description
-                                }))
-
-                                console.log(origin)
-                            }}
-                            fetchDetails={true}
-                            returnKeyType={'search'}
-                        />
-                    </View>
+                {/* Header Text */}
+                <View>
+                    <Text style={tw`text-3xl font-bold pt-6 text-gray-700`}>Trek Am?</Text>
+                    <Text style={tw`text-xl pt-2 text-gray-700`}>Measure the distance</Text>
                 </View>
-
-                {/* Where to */}
-                <View style={tw`mt-6 mb-10`}>
-                    <Text style={tw`text-xl text-gray-700 font-bold`}>Destination</Text>
-                    <View style={tw`mt-4 px-2`}>
-                        <GooglePlacesAutocomplete
-                            placeholder='Where To'
-                            nearbyPlacesAPI='GooglePlacesSearch'
-                            debounce={400}
-                            styles={inputBoxStyles}
-
-                            query={{
-                                key: 'AIzaSyBiH_uMgVwEkiVbs - kvSPEDd_NRzDzMdCE',
-                                language: 'en'
-                            }}
-                            minLength={2}
-                            enablePoweredByContainer={false}
-                            onPress={(data, details = null) => {
-                                dispatch(
-                                    setDestination({
-                                        location: details.geometry.location,
-                                        description: data.description,
-                                    }))
-                                console.log(destination)
-                            }}
-                            fetchDetails={true}
-                            returnKeyType={'search'}
-                        />
-                    </View>
-                </View>
-
-
-                {/* Calculate Distance button */}
-                <NavOptions />
-
             </View>
-        </ScrollView>
+
+
+            {/* Where from */}
+            <View style={tw`mt-4`}>
+                <Text style={tw`text-xl text-gray-700 font-bold`}>Starting from?</Text>
+                <View style={tw`mt-4 px-2`}>
+                    <GooglePlacesAutocomplete
+                        placeholder='Where From'
+                        nearbyPlacesAPI='GooglePlacesSearch'
+                        debounce={400}
+                        styles={inputBoxStyles}
+
+                        query={{
+                            key: 'AIzaSyBiH_uMgVwEkiVbs - kvSPEDd_NRzDzMdCE',
+                            language: 'en'
+                        }}
+                        minLength={2}
+                        enablePoweredByContainer={false}
+                        onPress={(data, details = null) => {
+                            dispatch(setOrigin({
+                                location: details.geometry.location,
+                                description: data.description
+                            }))
+
+                            console.log(origin)
+                        }}
+                        fetchDetails={true}
+                        returnKeyType={'search'}
+                    />
+                </View>
+            </View>
+
+            {/* Where to */}
+            <View style={tw`mt-6 mb-10`}>
+                <Text style={tw`text-xl text-gray-700 font-bold`}>Destination</Text>
+                <View style={tw`mt-4 px-2`}>
+                    <GooglePlacesAutocomplete
+                        placeholder='Where To'
+                        nearbyPlacesAPI='GooglePlacesSearch'
+                        debounce={400}
+                        styles={inputBoxStyles}
+
+                        query={{
+                            key: 'AIzaSyBiH_uMgVwEkiVbs - kvSPEDd_NRzDzMdCE',
+                            language: 'en'
+                        }}
+                        minLength={2}
+                        enablePoweredByContainer={false}
+                        onPress={(data, details = null) => {
+                            dispatch(
+                                setDestination({
+                                    location: details.geometry.location,
+                                    description: data.description,
+                                }))
+                            console.log(destination)
+                        }}
+                        fetchDetails={true}
+                        returnKeyType={'search'}
+                    />
+                </View>
+            </View>
+
+
+            {/* Calculate Distance button */}
+            <NavOptions />
+
+        </View>
 
     )
 }
--- screens/ResultScreen.js
+++ screens/ResultScreen.js
@@ -1,9 +1,121 @@
 import { View, Text } from 'react-native'
+import { selectDestination, selectOrigin, setTravelTimeInformation, selectTravelTimeInformation } from '../slices/navSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import tw from 'tailwind-react-native-classnames'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Icon } from '@rneui/themed'
+import { useNavigation } from '@react-navigation/native'
+import { ScrollView } from 'react-native'
+
+
+
 const ResultScreen = () => {
+
+    const navigation = useNavigation()
+
+    const origin = useSelector(selectOrigin)
+    const destination = useSelector(selectDestination)
+    const dispatch = useDispatch()
+
+
+    const travelTimeInformation = useSelector(selectTravelTimeInformation)
+
+    const travelDistance = travelTimeInformation?.distance.text
+    const travelValue = travelTimeInformation?.distance.value
+    const travelDuration = travelTimeInformation?.duration.text
+
+    const steps = Math.round(travelValue * 1.4);
+    const calories = Math.round(steps * 0.04);
+    console.log(travelTimeInformation);
+
+    useEffect(() => {
+        if (!origin || !destination) return
+
+        const getTravelTime = async () => {
+            fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?&mode=walking&destinations=${destination.description}&origins=${origin.description}&units=metric&key=AIzaSyBiH_uMgVwEkiVbs - kvSPEDd_NRzDzMdCE`)
+                .then((res) => res.json())
+                .then(data => {
+                    dispatch(setTravelTimeInformation(data.rows[0].elements[0]))
+                })
+        }
+        getTravelTime()
+
+    }, [origin, destination]);
+
     return (
-        <View>
-            <Text>ResultScreen</Text>
-        </View>
+        <ScrollView style={tw`bg-white flex-1`}>
+            <View style={tw`my-10`}>
+
+                <View style={tw`flex-row items-center w-full pr-5`}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('HomeScreen')}
+                        style={tw`rounded-full z-50`}>
+                        <Icon
+                            name='chevron-left'
+                            type='fontawesome'
+                            size={65}
+                        />
+                    </TouchableOpacity>
+                    <Text style={tw`text-3xl ml-14`}>Treking Info:</Text>
+                </View>
+
+                <View style={tw`justify-center mt-12`}>
+                    <TouchableOpacity
+                        style={tw`rounded-full z-50`}>
+                        <Icon
+                            name='sunrise'
+                            type='feather'
+                            size={65}
+                            color="blue"
+                        />
+                    </TouchableOpacity>
+                    <Text style={tw`text-center text-4xl mt-8 text-blue-600`}>{steps} Steps</Text>
+                </View>
+
+                {/* Icons */}
+                <View style={tw`mt-12 flex-row justify-between px-5`}>
+                    <View style={tw`flex-1`}>
+                        <TouchableOpacity
+                            style={tw`rounded-full z-50`}>
+                            <Icon
+                                name='target'
+                                type='feather'
+                                color="black"
+                                size={40}
+                            />
+                        </TouchableOpacity>
+                        <Text style={tw`text-center text-2xl mt-2 uppercase`}>{travelDistance}</Text>
+                    </View>
+                    <View style={tw`flex-1`}>
+                        <TouchableOpacity
+                            style={tw`rounded-full z-50`}>
+                            <Icon
+                                name='droplet'
+                                type='feather'
+                                size={40}
+                            />
+                        </TouchableOpacity>
+                        <Text style={tw`text-center text-2xl mt-2`}>{calories} Cal</Text>
+                    </View>
+                    <View style={tw`flex-1`}>
+                        <TouchableOpacity
+                            style={tw`rounded-full z-50`}>
+                            <Icon
+                                name='clock'
+                                type='feather'
+                                size={40}
+                            />
+                        </TouchableOpacity>
+                        <Text style={tw`text-center text-2xl mt-2`}>{travelDuration}</Text>
+                    </View>
+                </View>
+
+                <View>
+                </View>
+
+            </View>
+        </ScrollView>
     )
 }
 export default ResultScreen
